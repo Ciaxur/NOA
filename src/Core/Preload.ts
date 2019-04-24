587657010c27c8950d2c48bab6e9401603590a66
Omar <omar41097@gmail.com>
2019-04-23 22:10:54 -0400

    Updated Preload
    - Added Friends List to the Submenu as a first Menu
    - Still has Flaws (TODOs)
    - Displays Online Friends by appending to Side Menu

--- src/Core/Preload.ts
+++ src/Core/Preload.ts
@@ -3,7 +3,9 @@
  *  app.
  */
 import * as $ from 'jquery';
-import { sideMenuInit} from '../Client/SideMenu';
+import { SideMenu } from '../Client/SideMenu';
+import { CLIENT_DATA } from './Constants';
+import { ClientNode } from '../Client/ClientNode';
 
 
 /** 
@@ -16,5 +18,38 @@ $(document).ready(e => {
     document.getElementById("chatBox").focus();
 
     // Initiate Side Menu
-    sideMenuInit();
+    SideMenu.initSideMenu();
+
+    /** 
+     * TODO: Add a Friends List side menu when clicked,
+     *  Append sub-"lists" of the available users that are online :)
+     * - Have those users' status & part of the UID show (Grayed Out)
+     * 
+     * TODO: Add Some way to REMOVE when clicked again...
+     * 
+     * TODO: Make it to were if a user connects, ALL other users send their
+     *  "packet" to the user to update their friends list :)
+     */
+    SideMenu.appendSubMenu("Friends List", (e: Element) => {
+        // Get SubMenu Parent Element
+        const parent = e.parentElement;
+
+        // Get Client Node's Friends List
+        const clientNode = (CLIENT_DATA.node as ClientNode);
+        
+        // Append all Friends
+        const list = clientNode.getFriendsList();
+        for (let i = 0; i < list.length; i++) {
+            const div = document.createElement('div');
+            div.classList.add('menu-submenu');
+
+            // Construct Smaller UID
+            // Append to Friends List
+            const smallUID = list[i].UID.substr(list[i].UID.length - 4);
+            div.innerHTML = `${list[i].username} <span class="text-muted">(${smallUID})</span>`;
+
+            parent.appendChild(div);
+        }
+    });
+
 });
